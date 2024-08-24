d5a247e2e0f701980f3bf20ed31c2b8a23172331
Taaate <an981022@naver.com>
2024-08-24 14:20:06 +0900

    fix: post의 isDeleted 열 삭제

--- src/main/java/com/example/techlog/post/repository/PostRepository.java
+++ src/main/java/com/example/techlog/post/repository/PostRepository.java
@@ -1,10 +1,7 @@
 package com.example.techlog.post.repository;
 
 import com.example.techlog.post.domain.Post;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 

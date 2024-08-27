d9611787906726430969341517e2eee6b6435789
Taaate <an981022@naver.com>
2024-08-27 15:57:18 +0900

    refactor: 사용하지 않는 로직 제거

--- src/main/java/com/example/techlog/tag/repository/PostTagRepository.java
+++ src/main/java/com/example/techlog/tag/repository/PostTagRepository.java
@@ -1,7 +1,6 @@
 package com.example.techlog.tag.repository;
 
 import com.example.techlog.tag.domain.PostTag;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,8 +11,5 @@ public interface PostTagRepository extends JpaRepository<PostTag, Long> {
     @Modifying
     @Query("delete from PostTag pt where pt.post.id = :post")
     void deleteAllByPost(@Param("post") Long post);
-
-    @Query("select p.id from PostTag pt join fetch Post p on pt.post.id = p.id where pt.tag.id = :tagId")
-    List<Long> findByTagId(@Param("tagId") Long tagId);
 
 }
